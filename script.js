c8de8c61ce2740c7d29db6d3ee24d4ed27b62188
Jędrzej Bielicki <jedrzejb123@gmail.com>
2020-01-29 11:21:40 +0100

    change class names, add third section add new function

--- script.js
+++ script.js
@@ -1,48 +1,31 @@
 function parallax(element, distance, speed) {
     const item = document.querySelector(element);
-    item.style.transform = `translateY(${distance * speed}px)`;
+    item.style.transform = `translateY(-${distance * speed}px)`;
 }
-function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        var later = function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-};
+function parallax2(element, distance, speed) {
+    const item = document.querySelector(element);
+    item.style.transform = `translateY(-${(distance - h1) * speed}px)`;
+}
+
 window.addEventListener("scroll", function () {
     parallax(".first", window.scrollY, 0.4);
-    parallax(".second", window.scrollY, 0.1);
+    parallax(".second", window.scrollY, 0.7);
     parallax(".third", window.scrollY, 0.3);
     parallax(".fourth", window.scrollY, 0.2);
 })
+let header = document.querySelector('.first-section');
+let year = document.querySelector('.second-section');
+let h = year.clientHeight;
+let h1 = header.clientHeight;
+console.log(h);
 
-const year = document.querySelector('.about');
-
-
-
-function pxCounter() {
-
-    window.addEventListener("scroll", function () {
-        parallax(".y", window.scrollY, 0.1);
-        parallax(".e", window.scrollY, 0.5);
-        parallax(".a", window.scrollY, 0.3);
-        parallax(".r", window.scrollY, 0.2);
-    });
-
-}
-
-window.addEventListener('scroll', debounce(pxCounter));
-
-
-
-
-
-
-
+document.addEventListener('scroll', function(){
+    if(scrollY > h){
+        window.addEventListener("scroll", function () {
+            parallax2(".y", window.scrollY, 0.6);
+            parallax2(".e", window.scrollY, 0.4);
+            parallax2(".a", window.scrollY, 0.3);
+            parallax2(".r", window.scrollY, 0.5);
+        });
+    };
+})
